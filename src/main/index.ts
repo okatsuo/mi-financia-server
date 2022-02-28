37554b6419a6672750bdf1284447d42c27f9e6c2
foxlip <jingwenfeng454@hotmail.com>
2022-02-28 12:33:52 +0800

    first commit

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,47 @@
 console.log('Working')
+
+interface User {
+  id: string
+  name: string
+  email: string
+}
+
+const users: User[] = [{
+  id: '1',
+  name: 'Katsuo',
+  email: 'lovejingwen'
+},
+{
+  id: '2',
+  name: 'Jingwen',
+  email: 'lovekatsuo'
+}]
+
+const getUsers = (): User[] => {
+  return users
+}
+
+const getUser = (id: string): User | undefined => {
+  return users.find((user) => user.id === id)
+}
+
+const removeUser = (id: string): User | undefined => {
+  const index = users.findIndex((user) => user.id === id)
+  return users.slice(index, 1)[0]
+}
+
+const updateUser = (upUser: User): User => {
+  const index = users.findIndex((user) => user.id === upUser.id)
+  if (index < 0) throw new Error('Hey bro, we dont have that user')
+  users[index].name = upUser.name
+  users[index].email = upUser.email
+  return users[index]
+}
+getUsers()
+getUser('1')
+removeUser('3')
+console.log(updateUser({
+  id: '3',
+  name: 'Jingwen',
+  email: 'lovekatsuo'
+}))
